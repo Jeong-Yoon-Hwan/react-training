b10e405565a6628032c27e2a0af52f97dac28111
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2024-01-29 01:27:11 +0900

    토글 애니메이션 테스트

--- src/page/drag/index.tsx
+++ src/page/drag/index.tsx
@@ -1,9 +1,31 @@
 import styles from "./index.module.css";
+import { useRef, useState } from "react";
 
 export const DragPage = () => {
+  const container = useRef<HTMLDivElement | null>(null);
+  const [containerToggle, setContainerToggle] = useState<boolean>(false);
+
+  const moveContainer = () => {
+    console.log("ok");
+
+    if (container.current && containerToggle) {
+      container.current.style.scale = "1.5";
+      container.current.style.transition = "1s";
+      setContainerToggle(!containerToggle);
+    } else if (container.current && !containerToggle) {
+      container.current.style.scale = "1";
+      container.current.style.transition = "1s";
+      setContainerToggle(!containerToggle);
+    }
+  };
+
   return (
     <div className={styles.root}>
-      <div>Drag Page</div>
+      <div
+        ref={container}
+        onClick={moveContainer}
+        style={{ width: "100px", height: "100px", backgroundColor: "red" }}
+      ></div>
     </div>
   );
 };
